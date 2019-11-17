6e02174456d9426c23f2c17575286c2f1fce1910
purvimalik18 <54708626+purvimalik18@users.noreply.github.com>
2019-11-17 19:39:18 +0530

    Add files via upload

---
+++ notepad.py
@@ -0,0 +1,38 @@
+from tkinter import *
+#to save, read, write, etc
+import os
+
+m = Tk()  # to create window
+m.geometry("800x600")
+m.title('Untitled-Notepad')
+m.config(menu=m)
+menu = Menu(m)
+f = Menu(menu, tearoff=False)
+
+menu.add_cascade(label='File', menu=f)
+f.add_command(label='New', accelerator='Ctrl+N', command='f.__newFile')
+f.add_command(label='Open', accelerator='Ctrl+O', command='f.__openFile')
+f.add_command(label='Save', accelerator='Ctrl+S', command='f.__saveFile')
+f.add_separator()
+f.add_command(label='Exit', command=m.quit)
+
+e = Menu(menu, tearoff=False)
+menu.add_cascade(label='Edit', menu=e)
+e.add_command(label='Cut', command='e.__cut')
+e.add_command(label='Copy', command='e.__copy')
+e.add_command(label='Paste', command='e.__paste')
+
+c = Menu(menu, tearoff=False)
+menu.add_cascade(label='Commands', menu=c)
+c.add_command(label='About Commands', command='c.__showCommands')
+
+h = Menu(menu, tearoff=False)
+menu.add_cascade(label='Help', menu=h)
+h.add_command(label='About Notepad')
+
+m.config(menu=menu)  # to view all the cascades
+m.mainloop()  # to open and hold
+
+
+
+
